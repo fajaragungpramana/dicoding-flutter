5f8e9136fd41e32b1cc8f4c4b681900ee0bde253
fajar.agungpramana01 <fajar.agungpramana@mareco.id>
2022-06-03 11:01:58 +0700

    First Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,71 +8,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'FirstScreen',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Hello, World!"),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: IconButton(
+                icon: const Icon(Icons.menu, color: Colors.white), 
+                onPressed: () {}
             ),
-            body: const Center(
-                child: BiggerText(text: "Hello, World!")
-            )
+            title: const Text("FirstScreen"),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search, color: Colors.white),
+                onPressed: () {}
+              )
+            ]
+        ),
+        body: const Center(
+          child: Text("Hello, World!")
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
         )
-    );
+      );
   }
-}
-
-class Heading extends StatelessWidget {
-
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold
-      ),
-    );
-  }
-
-}
-
-class BiggerText extends StatefulWidget {
-
-  final String text;
-
-  const BiggerText({Key? key, required this.text}) : super(key: key);
-
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-
-}
-
-class _BiggerTextState extends State<BiggerText> {
-
-  double _textSize = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _textSize = 32;
-              });
-            },
-            child: const Text("Perbesar"))
-      ],
-    );
-  }
-
 }
