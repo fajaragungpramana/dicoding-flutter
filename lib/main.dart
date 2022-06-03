f11366ca45b5107b46fd40239600dce8faba0fc9
fajar.agungpramana01 <fajar.agungpramana@mareco.id>
2022-06-03 13:30:22 +0700

    Codelab 1.

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,72 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const FirstScreen());
+        home: const DetailScreen());
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-                icon: const Icon(Icons.menu, color: Colors.white), 
-                onPressed: () {}
-            ),
-            title: const Text("FirstScreen"),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.search, color: Colors.white),
-                onPressed: () {}
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                "Farm House Lembang",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+                ),
               )
-            ]
-        ),
-        body: const Center(
-          child: Text("Hello, World!")
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text("Open Everiday")
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.access_time),
+                      SizedBox(height: 8.0),
+                      Text("09:00 - 20:00")
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text("RP 25.000")
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0)
+                ),
+            )
+          ],
         )
-      );
+      )
+    );
   }
+
 }
